7f92fb897875e6b72a159e1940279a5c0050dde6
AbhijithS <80395140+Abhijith-Sudhakaran@users.noreply.github.com>
2021-10-14 20:18:58 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,24 @@
+# PLEASE EDIT THIS FILE IF YOU DEPLOYING THIS BOT ON VPS. ELSE *DON'T* TOUCH THIS FILE.
+
+# Please edit 'CONFIG = False' to 'CONFIG = True'
+CONFIG = False
+
+# Add your APP ID from my.telegram.org
+API_ID = 123456
+
+# Add your API HASH from my.telegram.org
+API_HASH = "xxxxxxxxxxxxxxxxxxx"
+
+# Read readme and generate string session. Then add that to here.
+SESSION = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+
+# Add your bot token here
+TOKEN = "12345678:xxxxxxxxxxxxxxxxxxx"
+
+# Chat id where bot should work.
+CHAT_ID = -10010000000
+
+# Add ur TG ID, Seperate by spaces
+ADMINS = 123456789
+
+#edit this lines when you deploying in VPS
