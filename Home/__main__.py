167284ee26709a453581974b3f21ce81f4953e82
AbhijithS <80395140+Abhijith-Sudhakaran@users.noreply.github.com>
2021-10-14 20:24:57 +0530

    Create __main__.py

---
+++ Home/__main__.py
@@ -0,0 +1,19 @@
+from Home import vcusr
+from Home import TOKEN, API_ID, API_HASH
+from pyrogram import Client
+import logging, os
+
+logging.basicConfig(level=logging.WARNING,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+Client(
+    "LS TG",
+    API_ID,
+    API_HASH,
+    bot_token=TOKEN,
+    plugins={'root': 'Home.plugins'}
+).start()
+os.system("echo 'Bot Started'")
+vcusr.run()
